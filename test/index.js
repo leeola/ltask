f0caeee372574a226e7d27be8123f053dc78a38d
Lee Olayvar <leeolayvar@gmail.com>
2012-03-02 14:49:20 +0000

    "The seq and par functions were said to return the wrong thing. Corrected."

--- test/index.js
+++ test/index.js
@@ -1,7 +1,22 @@
 #!/usr/bin/env node
 //
+// test/index.js
 //
+// Our main test file. Run this with node, and watch all the vows do stuff.
 //
 // @copyright 2012 by Lee Olayvar
 //
 /*jshint asi: true*/
+
+var tests = [
+  require('./ltask')
+]
+
+if (require.main == module) {
+  for (var i = 0; i < tests.length; i++) {
+    var suites = tests[i]
+    Object.keys(suites).forEach(function (suite_key) {
+      suites[suite_key].run()
+    })
+  }
+}
