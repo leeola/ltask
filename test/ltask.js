11d6bf6fb947e682129edc93bdf46d3af8346ee6
Lee Olayvar <leeolayvar@gmail.com>
2012-03-06 16:05:18 +0000

    "Experimenting with mocha tests, and fixed a bug due to those tests."

--- test/ltask.js
+++ test/ltask.js
@@ -15,3 +15,39 @@ var after = global.after,
     beforeEach = global.beforeEach,
     describe = global.describe,
     it = global.it
+
+
+describe('An ltask instance', function () {
+  var ltask = undefined
+  
+  describe('with no task on creation', function () {
+    before(function () {
+      ltask = new LTask()
+    })
+    
+    it('should be complete before it\'s started', function () {
+      ltask._task_started.should.equal(false)
+      ltask._task_completed.should.equal(false)
+    })
+    
+    it('should be complete after it\'s started', function () {
+      // Start the ltask
+      ltask.start()
+      // Note that we did not give it any asnyc functions, so this
+      // ltask is actually synchronous. So we should be able
+      // to test it in this manner.
+      ltask._task_started.should.equal(true)
+      ltask._task_completed.should.equal(true)
+    })
+  })
+  
+  describe('given a creation task', function () {
+    
+    it('should call the given task when started', function (done) {
+      new LTask(function () {
+        // Call our tests done function.
+        done()
+      }).start()
+    })
+  })
+})
